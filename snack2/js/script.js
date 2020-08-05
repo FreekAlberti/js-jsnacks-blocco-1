e9edc384f27efef98ba6bafd9069e74e2358729f
FreekAlberti <federicoalberi96@gmail.com>
2020-08-05 16:01:37 +0200

    implementato snack2

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,6 +1,8 @@
 var par1 = prompt("Inserisci una parola");
 var par2 = prompt("Inserisci una parola");
-if (par1.length > par2.length) {
+if ((par1 == "") || (par2 == "")) {
+  alert("Qualcosa é andato storto")
+} else if (par1.length > par2.length) {
   document.getElementById('parLunga').innerHTML = par1;
 } else if (par2.length > par1.length) {
   document.getElementById('parLunga').innerHTML = par2;
